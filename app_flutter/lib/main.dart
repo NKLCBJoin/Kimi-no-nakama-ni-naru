3fdfffbdd31f76ab1f986be454f3be1ad309a2b3
신근재 <60930743+Boknami@users.noreply.github.com>
2023-04-11 01:19:42 +0900

    게시판 개설요청 페이지 완료(디테일 추가해야)
    220411

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'board_main.dart';
 
-void main() {runApp(const Board_main());}
+void main() => runApp(MaterialApp(
+  title: 'Fluter_Comu',
+  theme: ThemeData(primarySwatch: Colors.blue),
+  home: Board_main(),
+));
 
 
