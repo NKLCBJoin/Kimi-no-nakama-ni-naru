3a86150bcdfb93ca4f1befd4cd5f411c23afc463
신근재 <60930743+Boknami@users.noreply.github.com>
2023-04-15 17:21:47 +0900

    이 주의 게시판 추가

---
+++ app_flutter/lib/View/board_hot_board.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class board_hot_board extends StatefulWidget {
+  const board_hot_board({Key? key}) : super(key: key);
+
+  @override
+  State<board_hot_board> createState() => _board_hot_boardState();
+}
+
+class _board_hot_boardState extends State<board_hot_board> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: EmptyAppBar(),
+          body: Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    SizedBox(height: 15,),
+
+                    Row(
+                      children: [
+                        Flexible(
+                            flex: 1,
+                            child: Image.asset('images/icons8-less-than-30.png', width: 35, height: 35,)
+                        ),
+
+                        Flexible(flex: 1, child: SizedBox(width: 100,)),
+
+                        Flexible(
+                            flex: 1,
+                            child: Text('이 주의 게시글', style: TextStyle(fontSize: 20, color: Colors.black),)
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 20,),
+                    Container(width: 1000, height: 2, decoration: BoxDecoration(color: Colors.black54),),
+
+                  ],
+                ),
+              )
+          ),
+        )
+    );
+  }
+}
+
+class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Size get preferredSize => Size(0.0, 0.0);
+}
