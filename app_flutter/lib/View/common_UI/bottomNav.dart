d0708036c39589f128f443d985b757ae21178443
신근재 <60930743+Boknami@users.noreply.github.com>
2023-05-06 19:33:10 +0900

    바텀 네비게이션 바

---
+++ app_flutter/lib/View/common_UI/bottomNav.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:you_my_colleague/View/alarm.dart';
+import 'package:you_my_colleague/View/board_main.dart';
+import 'package:you_my_colleague/View/settings.dart';
+import 'package:you_my_colleague/View/test.dart';
+
+//신근재
+class bottomNav extends StatefulWidget {
+  const bottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<bottomNav> createState() => _bottomNavState();
+}
+
+class _bottomNavState extends State<bottomNav> {
+  int _selectedIndex = 0;
+  final List<Widget> _children = <Widget>[
+    testPage(),
+    Board_main(),
+    alarmPage(),
+    SettingPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'no',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard),
+              label: 'no',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_active),
+              label: 'no',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'no',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+          onTap: _onItemTapped,
+      )
+    );
+  }
+}
