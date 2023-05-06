1d114cc721baa8ff71172a22b6099c3aaa4f7b33
신근재 <60930743+Boknami@users.noreply.github.com>
2023-05-06 19:32:59 +0900

    메인 구조 변경

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
-import 'View/board_main.dart';
+import 'package:you_my_colleague/View/common_UI/bottomNav.dart';
 
-void main() => runApp(MaterialApp(
-  title: 'Fluter_Comu',
-  theme: ThemeData(primarySwatch: Colors.blue),
-  home: Board_main(),
-));
+void main() {
+  runApp(Yamc());
+}
 
+class Yamc extends StatefulWidget {
+  const Yamc({Key? key}) : super(key: key);
 
+  @override
+  State<Yamc> createState() => _YamcState();
+}
+
+class _YamcState extends State<Yamc> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //bottomNav에서 페이지 선택함
+        home: bottomNav()
+    );
+  }
+}
